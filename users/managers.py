fe74fd0bfaf376845b195da4a39f62876ec56849
Abhinav Chavali <abhinavchavali12@gmail.com>
2023-06-06 12:00:27 -0400

    Added custom user model

--- users/managers.py
+++ users/managers.py
@@ -7,14 +7,14 @@ class CustomUserManager(BaseUserManager):
     """
 
     def create_user(self, email, username, password, **extra_fields):
-        if not email:
-            raise ValueError(_("Email field required"))
+        if not email or not username:
+            raise ValueError(_("Email and Username field required"))
 
         email = self.normalize_email(email)
         user = self.model(email=email, username=username, **extra_fields)
 
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         
         return user
 
--- users/models.py
+++ users/models.py
@@ -1,1 +1,20 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from .managers import CustomUserManager
 from django.db import models
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
